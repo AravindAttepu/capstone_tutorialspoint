f3e836003f90741259dc68b22408559ea929a48a
aravind <aravindattepu@gmail.com>
2025-09-06 12:03:56 +0530

    my first commit

---
+++ src/main/java/Pages/HomePage.java
@@ -0,0 +1,30 @@
+package Pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HomePage {
+
+	WebDriver driver= null;
+	By searchbar= By.xpath("//input[@name=\"search\"]");
+	By searchbtn= By.xpath("//button[@type=\"button\"]/i");
+	public HomePage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver = driver;
+		
+	}
+	public boolean searchproduct(String product)
+	{
+		driver.get("https://tutorialsninja.com/demo/index.php");
+		driver.findElement(searchbar).clear();
+		driver.findElement(searchbar).sendKeys(product);
+		driver.findElement(searchbtn).click();
+		return true;
+		
+	}
+	
+	
+}
---
+++ src/main/java/Pages/RegisterPage.java
@@ -0,0 +1,44 @@
+package Pages;
+
+import static org.testng.Assert.assertNotNull;
+
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import Utilities.WaitUtil;
+
+public class RegisterPage {
+
+	WebDriver driver=null;
+	By firstname = By.id("input-firstname");
+	By lastname= By.id("input-lastname");
+	By mail = By.id("input-email");
+	By telephone= By.id("input-telephone");
+	By password = By.id("input-password");
+	By cpassword = By.id("input-confirm");
+	By subscribe = By.xpath("//label[text()=\"Yes\"]");
+	By unsubscribe = By.xpath("//label[text()=\"No\"]");
+	By policy = By.xpath("//input[@type= \"checkbox\"]");
+	
+	public RegisterPage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver= driver;
+	}
+	
+	public boolean reigisteruser(Map<String, String> data)
+	{
+		driver.get("https://tutorialsninja.com/demo/index.php?route=account/register");
+		WaitUtil.waitForPageLoad(driver, 20);
+		driver.findElement(firstname).sendKeys(data.get("firstname"));
+		driver.findElement(lastname).sendKeys(data.get("lastname"));
+		driver.findElement(mail).sendKeys(data.get("mail"));
+		driver.findElement(telephone).sendKeys(data.get("phone"));
+		driver.findElement(password).sendKeys(data.get("password"));
+		driver.findElement(cpassword).sendKeys(data.get("password"));
+		driver.findElement(subscribe).click();
+		driver.findElement(policy).click();
+		return true;
+	}
+}
